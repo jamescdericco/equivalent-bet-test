09d9a28c433018e9d9b026dca3e3c9cf492c6a50
James De Ricco <jamescdericco@gmail.com>
2023-06-13 13:50:24 -0400

    ball_lottery: Set width to the squarish factor

--- app/ball_lottery.tsx
+++ app/ball_lottery.tsx
@@ -6,16 +6,35 @@ interface Props {
 
 export default function BallLottery({ odds }: Props) {
     /**
+     * Returns the largest factor that is less than or equal to the square root of the given number.
+     * 
+     * @param n Number to factor.
+     * @returns the largest factor that is less than or equal to the square root of the given number.
+     */
+    function squarishFactors(n: number): number {
+        const root = Math.sqrt(n);
+        for (let i = Math.floor(root); i > 0; i--) {
+            if (n % i === 0) {
+                return i;
+            }
+        }
+
+        throw new Error(`No squarish factors found for ${n}`);
+    }
+
+    /**
      * Returns JSX display of the lottery that has the given odds.
      * 
      * @param odds Odds of the lottery.
      * @param maxColumns Maximum number of columns to display. If null or zero, then all balls will be displayed in one row.
      */
-    function renderBalls(odds: Fraction, maxColumns: number | null = null) {
+    function renderBalls(odds: Fraction) {
         const numerator: number = odds.n;
         const denominator: number = odds.d;
         const jsx = [];
         const numBalls = numerator + denominator;
+        const squarishFactor = squarishFactors(numBalls);
+        const maxColumns = squarishFactor > 1 ? squarishFactor : null;
 
         let columnIndex = 0; // column index
 
@@ -38,7 +57,7 @@ export default function BallLottery({ odds }: Props) {
             </label>
 
             <div>
-                {renderBalls(odds, 40)}
+                {renderBalls(odds)}
             </div>
         </div>
     )
