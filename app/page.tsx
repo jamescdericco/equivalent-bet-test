188d6c1946ddc7d3c5c3065ebd9142a6b2c184de
James De Ricco <jamescdericco@gmail.com>
2023-06-19 16:28:49 -0400

    page: Add basic styling
    Centers the app on the page.
    Center the content within the app.
    Set the background of the app to 'lightgreen'.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import EquivalentBetTest from './equivalent_bet_test'
 export default function Home() {
   return (
     <main>
-      <div>
+      <div style={{
+        margin: 'auto', maxWidth: '620px', textAlign: 'center',
+        backgroundColor: 'lightgreen'
+      }}>
         <EquivalentBetTest />
       </div>
     </main>
