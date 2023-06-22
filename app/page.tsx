3141c2cfd7b12a744da2738f6e211dd3d5fd9f9e
James De Ricco <jamescdericco@gmail.com>
2023-06-22 15:47:52 -0400

    Remove lightgreen background color
    This avoids color conflicts with chakra-ui default theme for now.

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main>
       <div style={{
         margin: 'auto', maxWidth: '620px', textAlign: 'center',
-        backgroundColor: 'lightgreen', borderRadius: '10px', padding: '10px'
+        borderRadius: '10px', padding: '10px'
       }}>
         <EquivalentBetTest />
       </div>
